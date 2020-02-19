395de0c92440d32c66a2e4ce5b23d72910eaef48
Ron Martin <ronmartin89@gmail.com>
2020-02-19 15:06:05 -0800

    all tests working

--- test/app.spec.js
+++ test/app.spec.js
@@ -20,12 +20,22 @@ describe('App module', () => {
         .expect('Content-Type', /json/)
         .then(res => {
           expect(res.body).to.be.an('array');
-        })
-
-
+        });
     });
 
     // refactor this afterwards to use .forEach and replace "action" with a variable
-    it('should return correct apps for the genre "action"');
+    const validGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+    validGenres.forEach(item => {
+      it(`should return correct apps for the genre "${item}"`, () => {
+        return supertest(app)
+          .get('/apps')
+          .query({genres: item})
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .then(res => {
+            expect(res.body[0].Genres).to.include(item);
+          });
+      });
+    });
   });
 });
